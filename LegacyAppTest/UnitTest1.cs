b9ef840aa3610a2dd925a96cc8a716cd1ff4b755
Patryk Mikuli <patryk.mikuli@gmail.com>
2024-03-24 19:58:02 +0100

    Add tests

--- LegacyAppTest/UnitTest1.cs
+++ LegacyAppTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using LegacyApp;
+
 namespace LegacyAppTest
 {
     public class UnitTest1
@@ -6,10 +8,44 @@ namespace LegacyAppTest
         public void Test1()
         {
             // Arrange
+            var service = new UserService();
 
             // Act
+            var result = service.AddUser(null, null, "kowalski@wp.pl", new DateTime(1990, 1, 1), 1);
 
-            // Assert 
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void InvalidEmailNotAccepted()
+        {
+            var service = new UserService();
+
+            var result = service.AddUser("Jan", "Kowalski", "jankowlaskwppl", new DateTime(1990, 1, 1), 1);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TooYoungNotAccepted()
+        {
+            var service = new UserService();
+
+            var result = service.AddUser("Jan", "Kowalski", "jankowlaski.wp.pl", DateTime.Now, 1);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AddUser_ThrowsException_WhenUserDoesNotExist()
+        {
+            var service = new UserService();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = service.AddUser("Jan", "Kowalski", "jankowlaski.wp.pl", new DateTime(1990, 1, 1), 100);
+            });
         }
     }
 }
